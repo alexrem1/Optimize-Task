d9db966a532bf4a47c66412ed4715b381591a907
alexrem1 <remidy@live.co.uk>
2022-01-20 11:32:55 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+try {
+  // set widget to display none
+  document.querySelector(".mpx__widget--container").style.display = "none";
+
+  //mouseout listener added to window w/named function
+  window.addEventListener("mouseout", function delayShow(e) {
+    //check these conditions
+    const shouldShowExitIntent =
+      !e.toElement && !e.relatedTarget && e.clientY < 10;
+    //if they are all true then
+    if (shouldShowExitIntent) {
+      // delay displaying widget by 3s, remove eventlistener and scroll to top
+      setTimeout(() => {
+        this.window.removeEventListener("mouseout", delayShow);
+        this.window.scrollTo(0, 0);
+        document.querySelector(".mpx__widget--container").style.display =
+          "block";
+      });
+    }
+  });
+
+  // close widget
+  function closeWidget() {
+    document
+      .querySelector(".mpx__widget__header__close")
+      .addEventListener("click", () => {
+        document.querySelector(".mpx__widget--container").style.display =
+          "none";
+      });
+  }
+  //call close widget
+  closeWidget();
+} catch (error) {
+  console.log(error);
+}
